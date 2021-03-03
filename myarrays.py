abab9ded16ae3f1c232cdfc73241a3a36be87c59
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-03-03 14:16:19 -0600

    initial commits

---
+++ myarrays.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+integers = np.array([1, 2, 3])
+
+# print(type(integers))
+
+# One dimensional array
+list_num = np.array([x for x in range(2, 21) if x % 2 == 0])
+
+# print(list_num)
+
+# two dimensional array
+integers = np.array([[1, 2, 3], [4, 5, 6]])
+print(integers)
+
+floats = np.array([0.0, 0.1, 0.2, 0.3, 0.4])
+print(floats)
